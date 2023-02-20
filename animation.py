787883f4df2d7a0652ed5d64dad71b5f4c81b5ff
_ixmnxbdy111 <65754413+josephy2003@users.noreply.github.com>
2023-02-20 17:45:00 -0500

    20/2/23

--- animation.py
+++ animation.py
@@ -6,25 +6,52 @@ def animate():
     time.sleep(.5)
     print(".", end=" ")
     time.sleep(.5)
-    print(".", end=" ")
+    print("."
+          "", "\n")
+    print(""
+          
+          
+          "")
 
 animate()
 
-
-
-#intro message
-greetings_message_1 = "\nHello there! ;)"
+greetings_message_1 = "Hello there! ; )"
 greetings_message_2 = "I am a calculator which converts | Fahrenheit --> Celsius |"
-to_continue = "PRESS 'RETURN' TO CONTINUE"
-message = "*** Heat Index Calculator ***"
+to_continue = " PRESS 'RETURN' TO CONTINUE"
+message = "*** Heat Index Calculator ***" \
+          ""
 
 #instruction q's
 question_1 = "Please Enter A Number ***ANYWHERE BETWEEN 1-200***"
 
-ai_response = ['\nHello there! ;)',
-               'I am a calculator which converts | Fahrenheit --> Celsius |',
-               'PRESS *RETURN* TO CONTINUE', 'message = "*** Heat Index Calculator ***"']
+ai_responses = ['Hello there! ;)', 'PRESS *RETURN* TO CONTINUE', 'I am a calculator which converts | Fahrenheit --> Celsius |', '           *** Heat Index Calculator ***']
+
+ai_questions = ['How many locations?: ', 'Decimal precision for calculations [1--4]: ',
+                'Enter name of Location(s): ']
+
+def saysomething():
+    print(ai_responses[0])
+    time.sleep(1)
+    input(ai_responses[1])
+    time.sleep(1.5)
+    print(ai_responses[2], end="\n")
+    time.sleep(1)
+    print("\n", "\n", ai_responses[3], end=" ")
+    time.sleep(2.5)
+    return
+
+saysomething()
 
 
-print(*ai_response, sep=" ")
+    f_c = ['Enter air temperature [in deg F]', 'Enter relative humidity [in percentage]']
+
+
+def asksquestions():
+    print(""
+          "", "\n")
+    int(input(ai_questions[0]))
+    int(input(ai_questions[1]))
+    int(input(ai_questions[2]))
+
+asksquestions()
 
--- main.py
+++ main.py
@@ -1,26 +1,81 @@
 import time
-import animation
+
+def animate():
+    time.sleep(.5)
+    print(".", end=' ')
+    time.sleep(.5)
+    print(".", end=" ")
+    time.sleep(.5)
+    print("."
+          "", "\n")
+    print(""
+
+
+          "")
+
+
+animate()
 
 
 def pause():
     time.sleep(1)
 
 pause()
-animation
 
 
-def main():
-    print(greetings_message_1)
-    pause()
-    print(greetings_message_2)
+greetings_message_1 = "Hello there! ; )"
+greetings_message_2 = "I am a calculator which converts | Fahrenheit --> Celsius |"
+to_continue = " PRESS 'RETURN' TO CONTINUE"
+message = "*** Heat Index Calculator ***" \
+          ""
 
-main()
+# instruction q's
+question_1 = "Please Enter A Number ***ANYWHERE BETWEEN 1-200***"
+
+ai_responses = ['Hello there! ;)', 'PRESS *RETURN* TO CONTINUE',
+                'I am a calculator which converts | Fahrenheit --> Celsius |',
+                '           *** Heat Index Calculator ***']
+
+ai_questions = ['How many locations?: ', 'Decimal precision for calculations [1--4]: ',
+                'Enter name of Location(s): ']
+
+
+
+
+def saysomething():
+    print(ai_responses[0])
+    time.sleep(1)
+    input(ai_responses[1])
+    time.sleep(1.5)
+    print(ai_responses[2], end="\n")
+    time.sleep(1)
+    print("\n", "\n", ai_responses[3], end=" ")
+    time.sleep(2.5)
+    return
+
+
+saysomething()
+
+f_c = ['Enter air temperature [in deg F]', 'Enter relative humidity [in percentage]']
+
+
+def asksquestions():
+
+    int(input(ai_questions[0]))
+    int(input(ai_questions[1]))
+    int(input(ai_questions[2]))
+
+
+
+asksquestions()
 
 
 
 
 def prompt():
-    L = input("Please enter 3 locations.")
+    print(""
+            "", "\n")
+    L = input(int(input(ai_questions[0]))
     pause()
     LM = input("Then, the location name(s).")
     pause()
@@ -38,8 +93,6 @@ def prompt():
 
 prompt()
 
-
-    if__name__ == '__main__'
 
 
 
